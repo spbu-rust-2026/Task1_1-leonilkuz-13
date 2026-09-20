88f21ff03fadd435959154f0a12e70979693eda2
Leonid <leonilfilippenkov@gmail.com>
2026-09-20 20:03:12 +0300

    added solution

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use std::io::stdin;
+
 fn main() {
-    println!("Hello, world!");
+    let mut input = String::new();
+    println!("> Enter two numbers:");
+
+    stdin()
+        .read_line(&mut input)
+        .expect("> Failed to read the line");
+
+    let mut iter = input
+        .split_whitespace()
+        .map(|x| x.parse().expect("This is not a number"));
+
+    let numbers: [i32; 2] = [
+        iter.next().expect("First number not found"),
+        iter.next().expect("Second number not found"),
+    ];
+
+    let total: i32 = numbers.iter().sum();
+
+    println!("total: {}", total);
 }
